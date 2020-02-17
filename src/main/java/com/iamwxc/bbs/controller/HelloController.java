9f0f3b28e2e1acb1b13269299f99656c3c1c3b16
Eddy Wei <anlowee@outlook.com>
2020-02-17 16:50:08 +0800

    :construction:

--- src/main/java/com/iamwxc/bbs/controller/HelloController.java
+++ src/main/java/com/iamwxc/bbs/controller/HelloController.java
@@ -3,21 +3,10 @@ package com.iamwxc.bbs.controller;
 import com.iamwxc.bbs.dao.MomentDAO;
 import com.iamwxc.bbs.dao.MyUserDAO;
 import com.iamwxc.bbs.entity.MyUser;
-import com.iamwxc.bbs.entity.moment.Moment;
 import com.iamwxc.bbs.util.MyUserUtil;
-import net.bytebuddy.asm.Advice;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.session.FindByIndexNameSessionRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpSession;
-import java.sql.PseudoColumnUsage;
-import java.util.Enumeration;
-import java.util.List;
-import java.util.Optional;
 
 /**
  * Class description goes here.
