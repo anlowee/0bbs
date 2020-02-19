da5f0b0d2e43e4677ada9f5b3e25499a430d7500
Eddy Wei <anlowee@outlook.com>
2020-02-19 10:32:24 +0800

    :construction:
    add user profile custom.

---
+++ src/main/java/com/iamwxc/bbs/controller/ProfileManageController.java
@@ -0,0 +1,90 @@
+package com.iamwxc.bbs.controller;
+
+import com.iamwxc.bbs.dao.MyUserDAO;
+import com.iamwxc.bbs.entity.MyUser;
+import com.iamwxc.bbs.util.MyUserUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Class description goes here.
+ * <p>
+ * If you see this sentence, nothing ambiguous.
+ * </p>
+ *
+ * @author CC
+ * @version 1.0
+ */
+@RestController
+public class ProfileManageController {
+
+    @Autowired
+    private MyUserUtil myUserUtil;
+
+    @Autowired
+    private MyUserDAO myUserDAO;
+
+    @GetMapping("/profile")
+    public ModelAndView profile() {
+        ModelAndView modelAndView = new ModelAndView("profile");
+        MyUser currentUser = myUserUtil.getLoginUser();
+        modelAndView.addObject("username", currentUser.getUsername());
+        modelAndView.addObject("password", currentUser.getPassword());
+        modelAndView.addObject("email", currentUser.getEmailAddress());
+        modelAndView.addObject("profileURL", currentUser.getProfileURL());
+        return modelAndView;
+    }
+
+    @PostMapping("/profile")
+    public ModelAndView updateProfile(@RequestParam(value = "username", required = false) String newUsername,
+                                      @RequestParam(value = "password", required = false) String newPassword,
+                                      @RequestParam(value = "email", required = false) String newEmailAddress) {
+        ModelAndView modelAndView = new ModelAndView("profile");
+        boolean isUpdateUsernameOrPassword = false;
+        MyUser currentUser = myUserUtil.getLoginUser();
+
+        // update username
+        if (newUsername == null)
+            modelAndView.addObject("usernameNull", "用户名不能为空喵~");
+        else if (!newUsername.equals(currentUser.getUsername())) {
+            MyUser myUser = myUserDAO.findByUsername(newUsername);
+            if (myUser != null)
+                modelAndView.addObject("usernameExist", "用户名已存在喵~");
+            else {
+                currentUser.setUsername(newUsername);
+                isUpdateUsernameOrPassword = true;
+            }
+
+        }
+        // update password
+        if (newPassword == null)
+            modelAndView.addObject("passwordNull", "密码不能为空喵~");
+        else if (!newPassword.equals(currentUser.getPassword())) {
+            currentUser.setPassword(newPassword);
+            isUpdateUsernameOrPassword = true;
+        }
+        // update email addr
+        if (newEmailAddress != null) {
+            currentUser.setEmailAddress(newEmailAddress);
+            String MD5 = DigestUtils.md5DigestAsHex(newEmailAddress.getBytes());
+            String newProfileURL = "http://www.gravatar.com/avatar/" + MD5 + "?s=64";
+            currentUser.setProfileURL(newProfileURL);
+        }
+        myUserDAO.save(currentUser);
+
+        modelAndView.addObject("username", currentUser.getUsername());
+        modelAndView.addObject("password", currentUser.getPassword());
+        modelAndView.addObject("email", currentUser.getEmailAddress());
+        modelAndView.addObject("profileURL", currentUser.getProfileURL());
+        modelAndView.addObject("success", "相关信息已更新喵~");
+        if (isUpdateUsernameOrPassword)
+            modelAndView = new ModelAndView("redirect:/login");
+        return modelAndView;
+    }
+
+}
